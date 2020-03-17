8b6a1dea6b7ac56ae96d7f9647c0b3e2ac29899e
mscherer <mark.scherer@spryker.com>
2020-03-17 02:34:42 +0100

    Add plugin file

---
+++ src/Plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ajax;
+
+use Cake\Core\BasePlugin;
+use Cake\Routing\RouteBuilder;
+
+class Plugin extends BasePlugin {
+
+	/**
+	 * @var bool
+	 */
+	protected $middlewareEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $consoleEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $routesEnabled = false;
+
+}
